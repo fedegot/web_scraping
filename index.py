b4dc42e17bc58827cc1f9abda0e0af1ebe245f10
fedegot <gotfede@gmail.com>
2022-12-15 21:39:51 +0000

    load to database

--- index.py
+++ index.py
@@ -3,6 +3,8 @@ from selenium.webdriver.chrome.service import Service
 from bs4 import BeautifulSoup
 import pandas as pd  
 from decouple import config
+from sqlalchemy import create_engine
+
 
 ser = Service(config('DRIVER'))
 op = webdriver.ChromeOptions()
@@ -25,11 +27,10 @@ for a in soup.find_all("address", attrs={'class':'propertyCard-address property-
      appendo = a.get_text()
      area.append(appendo)
                     
-for j in soup.find_all("span", attrs={"class":"no-svg-bed-icon bed-icon seperator"}):
-     for s in j.select('title'):
+for a in soup.find_all("span", attrs={"class":"no-svg-bed-icon bed-icon seperator"}):
+     for s in a.select('title'):
         rooms.append(s.get_text())
     
-
 for a in soup.find_all('div', attrs={'class':'propertyCard-branchSummary property-card-updates'}):
      m = a.contents[1]
      first_agent.append(m)
@@ -39,15 +40,16 @@ for x in first_agent:
      mm.remove("by")        
      agent.append(' '.join(mm))
      
-      
-
-print(len(rooms))
-print(len(prices))
-print(len(area))
-print(len(agent))
-
-
+#load in a dataframe
 df = pd.DataFrame.from_dict({"Area":area, "Price": prices, "Rooms": rooms, "Agent": agent})
-df.to_csv("houses.csv",index = False, header=True, encoding='utf-8', sep='\t')
+#df.to_csv("houses.csv",index = False, header=True, encoding='utf-8', sep='\t')
+
+
+mydb = create_engine("mysql+pymysql://{user}:{pw}@localhost/{db}"
+                       .format(user="root",
+                               pw="",
+                               db="mysql"))
+
+df.to_sql('ciao', con = mydb, if_exists = 'append', chunksize = 1000)
 
 
